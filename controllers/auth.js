4eb8ea6b877f7eb43a3202145a2ae97cda035468
kostiukoleg <olegkostiuk@ukr.net>
2018-08-27 23:57:11 +0300

    finish auth controller

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,9 +1,52 @@
-module.exports.login = function (req,res) {res.status(200).json({
-    login: 
-    {
-        email: req.body.email,
-        password: req.body.password
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const keys = require('../config/keys.js');
+const User = require('../models/User.js');
+module.exports.login = async function (req,res) {
+    const candidate = await User.findOne({
+        email: req.body.email
+    });
+    if(candidate){
+        const passwordResult = bcrypt.compareSync(req.body.password, candidate.password);
+        if(passwordResult){
+            const token = jwt.sign({
+                email: candidate.email,
+                id: candidate._id
+            }, keys.jwt, {expiresIn:3600});
+            res.status(200).json({
+                token: `Bearer ${token}`
+            });
+        } else {
+            res.status(401).json({
+                message: 'Пароли не совпадают, попробуйте снова.'
+            });
+        }
+    } else {
+        res.status(404).json({
+            message: 'Пользователь с таким email не найден.'
+        });
     }
-});}
-
-module.exports.register = function (req,res) {res.status(200).json({register: "from controller"});}
+}
+module.exports.register = async function (req,res) {
+    const candidate = await User.findOne({
+        email: req.body.email
+    });
+    if (candidate) {
+        res.status(409).json({
+            message: 'Такой email уже зайнят, попробуйте другой.'
+        });
+    } else {
+        const salt = bcrypt.genSaltSync(10);
+        const password = req.body.password;
+        const user = new User({
+        email: req.body.email,
+            password: bcrypt.hashSync(password, salt)
+        }); 
+        try {
+            await user.save();
+            res.status(201).json(user);
+        } catch (e) {
+            console.log('Обработать ошибку'.e);
+        }
+    }
+    }
