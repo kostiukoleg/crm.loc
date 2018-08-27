6a320b86216a6e9154db24d864292c2b03d54ea2
kostiukoleg <olegkostiuk@ukr.net>
2018-08-28 00:11:24 +0300

    passport

--- middleware/passport.js
+++ middleware/passport.js
@@ -1,6 +1,28 @@
 const JwtStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
+const mongoose = require('mongoose');
+const User = mongoose.model('users');
+const keys = require('../config/keys.js');
 
 const options = {
-    jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken
+    jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+    secretOrKey: keys.jwt
 };
+
+module.exports = function(passport) {
+    passport.use(
+        new JwtStrategy(options, async function(payload, done){
+            try {
+                const user = await User.findById(payload.userId).select('email id');
+                
+                if (user) {
+                    done(null, user);
+                } else {
+                    done(null, false);
+                }
+            } catch(e) {
+                console.log(e);
+            }
+        });
+    );
+}
