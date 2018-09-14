0c929d7cb17bfe5b3c0fbc092f2e35d3fa2fc8a6
kostiukoleg <olegkostiuk@ukr.net>
2018-09-15 00:45:23 +0300

    half of lesson 36

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,7 +4,7 @@ import { AppRoutingModule } from './app.routing.module';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { LoginPageComponent } from './login-page/login-page.component';
 import { AuthLayoutComponent } from './shared/layouts/auth-layout/auth-layout.component';
 import { SiteLayoutComponent } from './shared/layouts/site-layout/site-layout.component';
@@ -21,10 +21,11 @@ import { RegisterPageComponent } from './register-page/register-page.component';
   imports: [
     BrowserModule,
     FormsModule,
+    ReactiveFormsModule,
     AppRoutingModule,
     NgbModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
--- client/src/app/login-page/login-page.component.ts
+++ client/src/app/login-page/login-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login-page',
@@ -6,17 +7,31 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
   styleUrls: ['./login-page.component.css']
 })
 export class LoginPageComponent implements OnInit {
+  
+  form: FormGroup
+  
   constructor() { }
 
   //Add the class to body tag when the View is initialized
   ngOnInit() {
     let body = document.getElementsByTagName('body')[0];
     body.classList.add("bg-dark");
+
+    this.form = new FormGroup(
+      {
+        email: new FormControl(null, [Validators.required, Validators.email]),
+        password: new FormControl(null, [Validators.required, Validators.minLength(6)])
+      }
+    )
   }
 
   //Remove the class from body tag when the View is destroyed
   ngOnDestroy() {
     let body = document.getElementsByTagName('body')[0];
     body.classList.remove("bg-dark");
+  }
+
+  onSubmit() { 
+    
   }
 }
